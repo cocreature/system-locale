28ca11fdcdc95840adec6da363022d3349b39811
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-06-03 20:31:13 +0200

    Switch to attoparsec

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
+# 0.2.0.0
+
+* Switch to attoparsec to reduce dependency footprint
+
 # 0.1.0.0 initial release
--- src/System/Locale/Read.hs
+++ src/System/Locale/Read.hs
@@ -1,46 +1,49 @@
 {-| Read locales on unix systems and parse them into their corresponding 'TimeLocale' representation.
 -}
 module System.Locale.Read
-  (getLocale
-  ,getCurrentLocale
-  ,parseLocale
-  ,TimeLocale(..)
-  ,LocaleParseException(..)
+  ( getLocale
+  , getCurrentLocale
+  , parseLocale
+  , TimeLocale(..)
+  , LocaleParseException(..)
   ) where
 
-import Control.Exception
-import Data.Time.Format (TimeLocale(..))
-import System.Process
-import Text.Megaparsec
-import Text.Megaparsec.String
+import           Control.Applicative
+import           Control.Exception
+import           Data.Attoparsec.Text
+import qualified Data.Text as Text
+import           Data.Time.Format (TimeLocale(..))
+import           System.Process
 
 -- | Thrown when the locale cannot be parsed
 data LocaleParseException =
-  LocaleParseException (ParseError Char Dec)
+  LocaleParseException String
   deriving (Show,Eq)
 
 instance Exception LocaleParseException
 
 -- | 'Parser' for locales returned by the unix utility 'locale'
 parseLocale :: Parser TimeLocale
-parseLocale =
-  do abDay <- parseSemicolonSeparatedLine
-     day <- parseSemicolonSeparatedLine
-     abMon <- parseSemicolonSeparatedLine
-     mon <- parseSemicolonSeparatedLine
-     [am,pm] <- parseSemicolonSeparatedLine
-     dateTimeFmt' <- manyTill anyChar newline
-     dateFmt' <- manyTill anyChar newline
-     timeFmt' <- manyTill anyChar newline
-     time12Fmt' <- manyTill anyChar newline
-     pure (TimeLocale (zip day abDay)
-                      (zip mon abMon)
-                      (am,pm)
-                      dateTimeFmt'
-                      dateFmt'
-                      timeFmt'
-                      time12Fmt'
-                      [])
+parseLocale = do
+  abDay <- parseSemicolonSeparatedLine
+  day <- parseSemicolonSeparatedLine
+  abMon <- parseSemicolonSeparatedLine
+  mon <- parseSemicolonSeparatedLine
+  [am, pm] <- parseSemicolonSeparatedLine
+  dateTimeFmt' <- manyTill anyChar newline
+  dateFmt' <- manyTill anyChar newline
+  timeFmt' <- manyTill anyChar newline
+  time12Fmt' <- manyTill anyChar newline
+  pure
+    (TimeLocale
+       (zip day abDay)
+       (zip mon abMon)
+       (am, pm)
+       dateTimeFmt'
+       dateFmt'
+       timeFmt'
+       time12Fmt'
+       [])
 
 -- | Read a locale with 'LC_TIME' set according to the first argument.
 --
@@ -51,13 +54,11 @@ parseLocale =
 --
 -- > getLocale (Just "en_US.UTF-8")
 getLocale :: Maybe String -> IO TimeLocale
-getLocale localeName =
-  do output <-
-       readCreateProcess (getLocaleProcess localeName)
-                         ""
-     case runParser parseLocale "" output of
-       Left err -> throwIO (LocaleParseException err)
-       Right locale -> pure locale
+getLocale localeName = do
+  output <- readCreateProcess (getLocaleProcess localeName) ""
+  case parseOnly (parseLocale <* endOfInput) (Text.pack output) of
+    Left err -> throwIO (LocaleParseException err)
+    Right locale -> pure locale
 
 -- | Get the current locale of the process.
 --
@@ -81,11 +82,14 @@ getLocaleProcess localeName =
         ,"t_fmt"
         ,"t_fmt_ampm"]) {env = toLangEnv <$> localeName}
 
+newline :: Parser Char
+newline = char '\n'
+
 parseSemicolonSeparatedLine :: Parser [String]
 parseSemicolonSeparatedLine =
-  sepBy (many (noneOf [';','\n']))
-        (char ';') <*
-  newline
+  sepBy (many (satisfy (not . finalChar))) (char ';') <* newline
+  where
+    finalChar c = c == ';' || c == '\n'
 
 toLangEnv :: String -> [(String,String)]
 toLangEnv s = [("LC_TIME",s)]
